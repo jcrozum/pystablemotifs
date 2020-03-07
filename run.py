29e99e943abcad1b65a34d8b301d9463dbe676a1
Reka Albert <reka.albert@gmail.com>
2020-03-07 14:47:03 -0500

    run.py ignores comments in model files

--- run.py
+++ run.py
@@ -2,14 +2,26 @@ import PyBoolNet
 import StableMotifs as sm
 import sys
 
-rules = open(sys.argv[1]).read()
+def parse(stream):
+    "Ingores commented out lines"
+    lines = list(stream)
+    lines = filter(lambda x: not x.startswith("#"), lines)
+    rules = "\n".join(lines)
+    return rules
 
+rules = parse(open(sys.argv[1]))
+
+# Reformat rules.
 rules_pbn = sm.Format.booleannet2bnet(rules)
+
+#
 primes = PyBoolNet.FileExchange.bnet2primes(rules_pbn)
-PyBoolNet.PrimeImplicants._percolation(primes,True) # Get rid of constants
-print("TLGL Network (Fixed Inputs):")
+
+# Get rid of constants
+PyBoolNet.PrimeImplicants._percolation(primes,True)
+
 sm.Format.pretty_print_prime_rules(primes)
-print()
 
 diag = sm.Succession.build_succession_diagram(primes)
+
 diag.attractor_candidate_summary()
