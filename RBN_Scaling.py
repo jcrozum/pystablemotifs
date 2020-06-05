8e2d6886e542d489a376db6d0011cef5b9bb7878
jcrozum <jcrozum@gmail.com>
2020-06-05 13:26:29 -0400

    Small tweaks to attractor scaling test template

--- RBN_Scaling.py
+++ RBN_Scaling.py
@@ -4,11 +4,11 @@ import StableMotifs as sm
 
 K=2 # in-degree
 p=sm.RandomBooleanNetworks.get_criticality_p_Kauffman(K)[0] # bias
-N_ensemble=1 # ensemble size
+N_ensemble=5 # ensemble size
 seed=1000
 followups = {}
 attbounds = {}
-for N in [128]:#[32,64,128,256,512]: # Number of nodes
+for N in [32,64,128,256,512]: # Number of nodes
     print("Generating ensemble Kauffman RBNs for N =",N,". . .")
     rbn_ensemble_rules=sm.RandomBooleanNetworks.Random_Boolean_Network_Ensemble_Kauffman(N,K,p,N_ensemble,seed=seed,write_Boolean_network=False)
     print("Ensemble generated.")
@@ -20,7 +20,7 @@ for N in [128]:#[32,64,128,256,512]: # Number of nodes
 
         ar = sm.AttractorRepertoire.from_primes(primes,max_simulate_size=10)
         attbounds[N].append((ar.fewest_attractors,ar.most_attractors))
-        
+
         if ar.fewest_attractors != ar.most_attractors:
             followups[N].append(ar)
         print(ar.fewest_attractors,ar.most_attractors)
