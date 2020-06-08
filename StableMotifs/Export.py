3bb7624e45b27fec940df51520d71f4f0a5eb818
David <dderitei@akceso.ch>
2020-06-08 12:17:12 +0200

    Added Export.py which contains the functions to help interpret and export the data. In this first commit it includes the functions networkx_succession_diagram_reduced_network_based(), plot_nx_succession_diagram(),networkx_succession_diagram_motif_based(),format_reduction_label()

---
+++ StableMotifs/Export.py
@@ -0,0 +1,78 @@
+import StableMotifs as sm
+import networkx as nx
+
+def format_reduction_label(s):
+    return s.replace("'","").replace('[','').replace(']','')
+
+def networkx_succession_diagram_reduced_network_based(ar,include_attractors_in_diagram=True):
+
+    '''
+    tbd
+    '''
+
+    G_reduced_network_based=ar.succession_diagram.digraph.copy()
+    for i in G_reduced_network_based.nodes():
+        G_reduced_network_based.nodes[i]['label']=format_reduction_label(str(ar.succession_diagram.motif_reduction_dict[i].motif_history))
+
+    if include_attractors_in_diagram:
+        for a_index,a in enumerate(ar.attractors):
+            G_reduced_network_based.add_node('A'+str(a_index))
+            G_reduced_network_based.nodes['A'+str(a_index)]['label']=format_reduction_label(str(a.attractor_dict))
+            for r in a.reductions:
+                r_key=list(ar.succession_diagram.motif_reduction_dict.keys())[list(ar.succession_diagram.motif_reduction_dict.values()).index(r)]
+                G_reduced_network_based.add_edge(r_key,'A'+str(a_index))
+
+    return G_reduced_network_based
+
+def plot_nx_succession_diagram(g, fig_dimensions=[], pos='pydot', detailed_labels=True, node_size=[], node_color='grey',
+                              font_size=12, label_color='b', font_color='black'):
+
+    '''
+    tbd
+    '''
+
+    from networkx.drawing.nx_agraph import graphviz_layout
+    import matplotlib.pyplot as plt
+
+    if fig_dimensions==[]:
+        fig_dimensions=(2*(g.number_of_nodes()+2),g.number_of_nodes()+2)
+    if node_size==[]:
+        node_size=50*g.number_of_nodes()
+
+    if pos=='pydot':
+        pos=graphviz_layout(g, prog='dot')
+
+    plt.figure(figsize=fig_dimensions)
+    nx.drawing.draw_networkx_nodes(g, pos,node_shape='s',node_color=node_color, node_size=node_size)
+    nx.draw_networkx_edges(g, pos, arrowstyle='fancy',arrowsize=20)
+    if detailed_labels:
+        nx.drawing.draw_networkx_labels(g,pos, labels=dict(g.nodes('label')),font_size=font_size, font_color=font_color)
+    else:
+        nx.drawing.draw_networkx_labels(g,pos,font_size=font_size, font_color=font_color)
+    plt.axis('off')
+    plt.show()
+
+def networkx_succession_diagram_motif_based(ar,include_attractors_in_diagram=True):
+
+    '''
+    tbd
+    '''
+    G_reduced_network_based=networkx_succession_diagram_reduced_network_based(ar,include_attractors_in_diagram=False)
+    G_motif_based = nx.line_graph(G_reduced_network_based)
+    for i,j in G_motif_based.nodes():
+
+
+        node_motif=set([frozenset(k.items()) for k in ar.succession_diagram.motif_reduction_dict[j].motif_history])-set([frozenset(k.items()) for k in ar.succession_diagram.motif_reduction_dict[i].motif_history])
+        node_label=format_reduction_label(str(dict(list(node_motif)[0])))
+        G_motif_based.nodes[(i,j)]['label']=node_label
+
+    if include_attractors_in_diagram:
+        for a_index,a in enumerate(ar.attractors):
+            G_motif_based.add_node('A'+str(a_index))
+            G_motif_based.nodes['A'+str(a_index)]['label']=format_reduction_label(str(a.attractor_dict))
+            for r in a.reductions:
+                r_key=list(ar.succession_diagram.motif_reduction_dict.keys())[list(ar.succession_diagram.motif_reduction_dict.values()).index(r)]
+                for i,j in G_motif_based.nodes():
+                    if r_key==j:
+                        G_motif_based.add_edge((i,j),'A'+str(a_index))
+    return G_motif_based
