e0b5361c3c41a0d4b4fedce247429454ff25ee2a
Kyu Hyong Park <kjp5774@psu.edu>
2021-07-14 11:13:08 -0400

    test MPBN update

---
+++ Testing/BasicFunctionsExample.py
@@ -0,0 +1,59 @@
+import PyBoolNet
+import PyStableMotifs as sm
+import networkx as nx
+print("Loading network . . .")
+primes = sm.Format.import_primes('./models/MPBN_test1.txt')
+print("Network loaded.")
+print()
+print("RULES")
+sm.Format.pretty_print_prime_rules({k:primes[k] for k in sorted(primes)})
+
+import time
+start_time = time.time()
+
+print()
+print("Analyzing network . . .")
+ar = sm.AttractorRepertoire.from_primes(primes,max_simulate_size=7)
+print("Analysis complete.")
+print()
+ar.summary()
+
+print("--- %s seconds for non-MBPN ---" % (time.time() - start_time))
+start_time = time.time()
+
+print()
+print("Analyzing network (MPBN) . . .")
+ax = sm.AttractorRepertoire.from_primes(primes,max_simulate_size=7,MPBN_update=True)
+print("Analysis complete.")
+print()
+ax.summary()
+
+print("--- %s seconds for MPBN ---" % (time.time() - start_time))
+
+#
+# print("Time reversal:")
+# tr = sm.TimeReversal.time_reverse_primes(primes)
+#
+# sm.Format.pretty_print_prime_rules(tr)
+#
+# print("\nTR Stable Notifs:")
+#
+# for x in ar.succession_diagram.motif_reduction_dict[0].stable_motifs:
+#     print(x)
+#
+# print("Producing and exporting STG . . .")
+# G=PyBoolNet.StateTransitionGraphs.primes2stg(primes,'asynchronous')
+# H=nx.DiGraph()
+# for u,v in G.edges():
+#     H.add_edge(u,v)
+# for n in H.nodes():
+#     H.nodes[n]["label"]=n
+# nx.write_graphml(H,"Fig3STG.graphml")
+# print("done.")
+#
+# print("Producing and exporting expanded network . . .")
+# K = sm.Export.expanded_network(primes)
+# KTR = sm.Export.expanded_network(sm.TimeReversal.time_reverse_primes(primes))
+# nx.write_graphml(K,"EN_PhaseSwitch.graphml")
+# nx.write_graphml(KTR,"EN_PhaseSwitch_TR.graphml")
+# print("done.")
